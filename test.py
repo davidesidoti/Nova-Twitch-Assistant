14e0961686678c2eee192710cab5118bafda2b21
hash_developer <63818411+davidesidoti@users.noreply.github.com>
2023-05-18 18:00:39 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,30 +1,23 @@
-import spotipy
-from spotipy.oauth2 import SpotifyOAuth
-import os
-from dotenv import load_dotenv
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
 
-load_dotenv()
+# Create a chatbot
+chatbot = ChatBot("myBot")
 
-client_id = os.getenv("SPOTIPY_CLIENT_ID")
-client_secret = os.getenv("SPOTIPY_CLIENT_SECRET")
-redirect_uri = os.getenv("SPOTIPY_REDIRECT_URI")
+# Train the chatbot using the corpus
+trainer = ChatterBotCorpusTrainer(chatbot)
+trainer.train("chatterbot.corpus.english.greetings")
 
-# Set up authentication
-scope = "user-library-read playlist-read-private playlist-read-collaborative user-read-private user-read-email user-read-playback-state user-modify-playback-state user-read-currently-playing"
-sp = SpotifyOAuth(client_id=client_id, client_secret=client_secret,
-                  redirect_uri=redirect_uri, scope=scope)
+# Start the conversation
+while True:
+    try:
+        user_input = input("You: ")
+        bot_response = chatbot.get_response(user_input)
+        print("Bot: ", bot_response)
 
-# Get the user's playlists
-results = sp.user_playlists('sidotidavide')
-# Loop through each playlist and print its name
-for playlist in results['items']:
-    print(playlist['name'])
+    # Exit the conversation with 'bye'
+        if user_input.lower() == 'bye':
+            break
 
-# Get the currently playing song
-try:
-    current_track = sp.current_playback()
-    track_name = current_track['item']['name']
-    artist_name = current_track['item']['artists'][0]['name']
-    print("Currently playing:", track_name, "by", artist_name)
-except:
-    print("No track currently playing.")
+    except (KeyboardInterrupt, EOFError, SystemExit):
+        break
